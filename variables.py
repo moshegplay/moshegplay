a3987f0961998f4eba88f6dd4575ebcab3dfa36c
moshegplay <moshegplay@gmail.com>
2021-04-17 03:10:29 +0300

    Update variables.py

---
+++ variables.py
@@ -0,0 +1,6 @@
+name="moshe hazan"
+age=29
+mail="moshe@gmail.com"
+print("full name: " + name +"\nage: " + str(age) +"\nmail:" + mail)
+print("full name: " + name[::-1] +"\nage: " + str(age*3))
+print("moshe" in "idan ben dudu moshe shimon yeal gal adam shahar yana")
