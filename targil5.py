5d1a317d44248913f9c631cf46e7dfe1dab0be9c
moshegplay <moshegplay@gmail.com>
2021-04-20 23:30:23 +0300

    Update targil5.py

--- targil5.py
+++ targil5.py
@@ -1,6 +1,6 @@
 #dictionary
-'''
-my_dict={"www.google.com":"8.8.8.8","www.facebook.com":"7.7.7.7","www.youtube.com":["5.5.5.5","4.4.4.4"]}
+
+my_dict={"www.google.com":" 142.250.187.196","www.facebook.com":"7.7.7.7","www.youtube.com":["5.5.5.5","4.4.4.4"]}
 my_dict2={"www.net4uc.com":"3.3.3.3","www.groo.co.il":"88.88.88.88"}
 print(my_dict)
 print(my_dict2)
@@ -10,10 +10,10 @@ print(my_dict)
 print("number of entries:" +str(len(my_dict)))
 print(my_dict["www.google.com"])
 print(my_dict.values())
-my_dict["www.google.com"]="8.8.4.4"
+#my_dict["www.google.com"]="8.8.4.4"
 print(my_dict["www.google.com"])
 print(my_dict)
-'''
+
 
 
 
