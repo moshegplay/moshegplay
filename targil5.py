08fc96c03a7f925c5e189a6c40ff8bcb93e73667
moshegplay <moshegplay@gmail.com>
2021-04-21 00:08:11 +0300

    Update targil5.py

--- targil5.py
+++ targil5.py
@@ -15,7 +15,5 @@ print(my_dict["www.google.com"])
 print(my_dict)
 '''
 
-
-
 my_dict={"www.google.com":"8.8.8.8","www.facebook.com":"7.7.7.7","www.youtube.com":["5.5.5.5","4.4.4.4"]}
 print("8.8.8.8" in my_dict.values())
