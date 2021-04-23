bd650fc8934e5f2d3d722310149783361b006893
moshegplay <moshegplay@gmail.com>
2021-04-23 19:33:26 +0300

    Create lists.py
    Co-Authored-By: moshegplay <82340451+moshegplay@users.noreply.github.com>

---
+++ LABs/lists.py
@@ -0,0 +1,15 @@
+details_list=["moshe hazan" , 29 , "moshe@gmail.com" , "050555000" ]
+print("Full name: " + details_list[0] + "\nAge: " + str(details_list[1])  + "\nMail: " + details_list[2] + "\nPhone: " + details_list[3] )
+ip_list=["1.1.1.1","2.2.2.2"]
+print(ip_list)
+ip_list.append("3.3.3.3")
+ip_list.append("4.4.4.4")
+ip_list.append("5.5.5.5")
+print(ip_list)
+ip_list.pop(2)
+print("ip list lenthis: " + str(len(ip_list)) + "\nAnd the IP list: " + str(ip_list))
+
+
+
+
+
